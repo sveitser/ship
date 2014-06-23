5778ac9a7967f52a0c9aa75590e7c6b6c55355b3
Mathis Antony <sveitser@gmail.com>
2014-06-23 18:42:35 +0800

    html output

--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 nosetests --with-xcoverage --with-xunit --cover-erase --cover-package=test\
           --xunit-file="${CIRCLE_ARTIFACTS}/nosetests.xml" \
-          --xcoverage-file="${CIRCLE_ARTIFACTS}/coverage.xml"
+          --xcoverage-file="${CIRCLE_ARTIFACTS}/coverage.xml" \
+          --cover-html-dir="${CIRCLE_ARTIFACTS}"
