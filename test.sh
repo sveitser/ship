df3f6dedd04946f72a74362f26124bc18a0cfec2
Mathis Antony <sveitser@gmail.com>
2014-06-23 18:34:55 +0800

    CIRCLE_ARTIFACTS

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 nosetests --with-xcoverage --with-xunit --cover-erase --cover-package=test\
-          --xunit-file="${BUILD_ARTIFACTS}/nosetests.xml" \
-          --xcoverage-file="${BUILD_ARTIFACTS}/coverage.xml"
+          --xunit-file="${CIRCLE_ARTIFACTS}/nosetests.xml" \
+          --xcoverage-file="${CIRCLE_ARTIFACTS}/coverage.xml"
